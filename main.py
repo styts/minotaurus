0feecfd0194c568a784abb2572d2b7ac9ebb68a2
styts <kirstyts@gmail.com>
2013-09-05 17:27:04 +0200

    assets/vhost templatified

--- main.py
+++ main.py
@@ -2,10 +2,13 @@
 
 from subprocess import call
 
-project_name = "myproject"
+project_name = "my_project"
+domain = "project.my"
 
 call(["django-admin.py",
       "startproject",
       "--template=project_template",
-      project_name])
+      project_name,
+      "--domain",
+      domain])
 call(["rm", "-rf", project_name])
