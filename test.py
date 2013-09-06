57100e0d387a2cc3fde4f327b8a9b0696fb61f79
styts <kirstyts@gmail.com>
2013-09-07 00:15:38 +0200

    some cleaning up

--- test.py
+++ test.py
@@ -1,39 +1,28 @@
 #!/usr/bin/env python
 
 import os
-import subprocess
 from fabric.api import local
 
 project_name = "example"
 
 
-def test_33():
-    pass
-
-
 def test_0_newproject():
-    # create project
-    #if not os.path.exists("./" + project_name):
-        #local('django-admin.py startproject --template=project_template ' +
-            #project_name)
+    # delete output if exists
     if os.path.exists("./" + project_name):
         local('rm -rf ' + project_name)
-    local('django-admin.py startproject --template=project_template ' +
+
+    # create project from template
+    local('django-admin.py startproject --template=templates/project ' +
         project_name)
 
-    # * run runserver
-    #local('python %s/manage.py runserver' % project_name)
-    ln = 'ln -s development.py\
+    # create settings link
+    ln_cmd = 'ln -s development.py\
           %(p)s/%(p)s/settings/__init__.py' % {"p": project_name}
-    print ln
-    local(ln)
+    local(ln_cmd)
+
+    # test that index page loads
     local('py.test %s/tests.py' % project_name)
-    #process = subprocess.Popen(['python',
-          #project_name,
-          #'manage.py',
-          #'runserver'],
-         #stdout=subprocess.PIPE)
-    #process.communicate()
+
     # * get http index
     # * check request content
 
@@ -41,4 +30,4 @@ def test_0_newproject():
     #assert "bootstrap.less" in content
 
     # this comes at the end
-    #local('rm -rf ' + project_name)
+    local('rm -rf ' + project_name)
