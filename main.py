f91eb4e7aec9ed577df9be938a94362206429b53
styts <kirstyts@gmail.com>
2013-09-06 19:48:29 +0200

    added 420 lines

--- main.py
+++ main.py
@@ -4,8 +4,10 @@ from subprocess import call
 
 project_name = "my_project"
 
-call(["django-admin.py",
-      "startproject",
-      "--template=project_template",
-      project_name])
-call(["rm", "-rf", project_name])
+def test_newproject():
+    """ i want to start a project"
+    call(["django-admin.py",
+        "startproject",
+        "--template=project_template",
+        project_name])
+    call(["rm", "-rf", project_name])
