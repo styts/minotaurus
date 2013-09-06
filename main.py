0000d0d5140d2f99f297ad771b73a61a444510fd
styts <kirstyts@gmail.com>
2013-09-06 21:27:32 +0200

    fou

--- main.py
+++ main.py
@@ -1,13 +1,31 @@
 #!/usr/bin/env python
 
+import os
+#import requ st
 from subprocess import call
+from fabric.api import local
+#from cuisine import file_link
 
 project_name = "my_project"
 
+
+def test_33():
+    pass
+
+
 def test_newproject():
-    """ i want to start a project"
-    call(["django-admin.py",
-        "startproject",
-        "--template=project_template",
-        project_name])
-    call(["rm", "-rf", project_name])
+    # create project
+    if not os.path.exists("./" + project_name):
+        local('django-admin.py startproject --template=project_template ' +
+            project_name)
+
+    # * run runserver
+    local('python %s/manage.py runserver' % project_name)
+    # * get http index
+    # * check request content
+
+    #content = request.content
+    #assert "bootstrap.less" in content
+
+    # this comes at the end
+    local('rm -rf ' + project_name)
